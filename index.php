4a403d865b7d024c89ea0910bf6dbe00349424be
Teo Zin Han <47917658+merctraider@users.noreply.github.com>
2024-02-25 16:21:24 +0800

    added select2 for hymns

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 
 			<div class="form-group">
 				<label>Opening Hymn:</label>
-				<select name="opening_hymn" class="form-control" required>
+				<select name="opening_hymn" class="form-control hymn" required>
 					<?php
 						$hymns = json_decode(file_get_contents('tlh.json'), true);
 
@@ -38,7 +38,7 @@
 
 			<div class="form-group">
 				<label>Chief Hymn:</label>
-				<select name="chief_hymn" class="form-control" required>
+				<select name="chief_hymn" class="form-control hymn" required>
 					<option value="default">Use Lectionary Hymn</option>
 					<?php
 						foreach ($hymns as $key => $hymn) {
@@ -82,6 +82,15 @@
 	<!-- Add Bootstrap JavaScript -->
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>\
+
+	<!--Add Select2 and dependencies-->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.7/js/select2.min.js"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.7/css/select2.min.css">
+	<script>
+		$(document).ready(function() {
+			$('select.hymn').select2();
+		});
+	</script>
 </body>
 </html>
